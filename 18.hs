a5fe8abb2bc81d726aa8c502eb8c8f16db1af8f5
Jeremy Davis <zinglons.ale@gmail.com>
2012-02-08 21:13:53 -0500

    har har

--- 18.hs
+++ 18.hs
@@ -1,3 +1,4 @@
 import Data.List
+--entering the actual problem data is left as an exercise for the reader
 rows = [[3],[7, 4],[2, 4, 6],[8, 5, 9, 3]]
 answer = foldr1 (\r1 r2 -> zipWith (+) r1 (zipWith max r2 (tail r2))) rows
