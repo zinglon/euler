40dceed09875d421c17bae27bec9700b460d3d3f
Jeremy Davis <zinglons.ale@gmail.com>
2012-02-18 22:37:44 -0500

    Compile with -Wsign-conversion -Wconversion just for kicks

--- 51.cc
+++ 51.cc
@@ -1,4 +1,4 @@
-// compiles cleanly with apple clang 3.0,  clang++ -O2 -Wall -Wextra -std=c++0x
+// compiles cleanly with apple clang 3.1,  clang++ -O2 -Wall -Wextra -std=c++11
 #include <iostream>
 #include <vector>
 #include <map>
@@ -12,14 +12,14 @@ using std::multimap;
 
 /* I use char all over just for the sake of premature space optimization */
 
-vector<vector<char>> choose(int n, int c) {
-  vector<vector<char>> ret;
+vector<vector<unsigned char>> choose(unsigned int n, unsigned int c) {
+  vector<vector<unsigned char>> ret;
   //well this is the worst way to do this other than anything else I came up with
-  for(int i = (1<<c) - 1; i < 1<<n; i++) {
-    if(__builtin_popcount(i) == c) {
-      int idx = 0;
-      int ii = i;
-      vector<char> idxs;
+  for(unsigned int i = (1<<c) - 1; i < 1<<n; i++) {
+    if((unsigned int)__builtin_popcount(i) == c) {
+      unsigned char idx = 0;
+      unsigned int ii = i;
+      vector<unsigned char> idxs;
       while(ii) {
 	if(ii & 1) 
 	  idxs.push_back(idx);
@@ -32,32 +32,32 @@ vector<vector<char>> choose(int n, int c) {
   return ret;
 }
 
-int powerof10(int power) {
-  static int values[] = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };
+unsigned int powerof10(unsigned int power) {
+  static unsigned int values[] = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };
   return values[power];
 }
 
 //digits should be in ascending order
 template<typename T>
-int chop(int x, T const& digits) {
-  int removed = 0;
+unsigned int chop(unsigned int x, T const& digits) {
+  unsigned int removed = 0;
   for(auto d : digits) {
-    int power = powerof10(d - removed);
+    auto power = powerof10(d - removed);
     x = (x % power) + (x / (power * 10)) * power;
     removed++;
   } 
   return x;
 }
 
-vector<pair<int, vector<char>>> make_keys(int num) {
-  auto digits = vector<vector<char>>(10,vector<char>());
-  auto index = 0;
+vector<pair<unsigned int, vector<unsigned char>>> make_keys(unsigned int num) {
+  auto digits = vector<vector<unsigned char>>(10,vector<unsigned char>());
+  unsigned char index = 0;
 
   //not strictly necessary, just felt like having the early-bailout hatch
   bool good = false;
 
   //record the indexes of the digits
-  int tempnum = num;
+  unsigned int tempnum = num;
   while(tempnum) {
     digits[tempnum%10].push_back(index);
     if(digits[tempnum%10].size() > 0) good = true;
@@ -70,10 +70,10 @@ vector<pair<int, vector<char>>> make_keys(int num) {
 
   //for each digit that occurs twice or more,
   // make a wildcarded version by removing 2+ occurrences and remembering the indexes
-  for(int digit = 0; digit < 10; digit++) {
+  for(unsigned int digit = 0; digit < 10; digit++) {
     auto const& idxs = digits[digit];
-    for(int remove = 2; remove <= (int)idxs.size(); remove++) {
-      auto dropidxs = choose(idxs.size(), remove);
+    for(unsigned int remove = 2; remove <= (unsigned int)idxs.size(); remove++) {
+      auto dropidxs = choose((unsigned int)idxs.size(), remove);
       for(auto& drops : dropidxs) {
 	for(auto& drop : drops) //quickly twiddle the actual indexes in
 	  drop = idxs[drop];
@@ -96,15 +96,15 @@ int main() {
 
   //SIEVE OF ERATOSTHENES TIME YEEEEAAAAHHHHH
   //let's try 4 million
-  const int MAX = 4000000;
-  const int SQRT_MAX = std::sqrt(MAX);
-  const int goal = 8;
+  const auto MAX = 4000000U;
+  const auto SQRT_MAX = (unsigned int)std::sqrt(MAX);
+  const auto goal = 8U;
   //[3,5..]
-  auto prim = vector<char>(MAX/2, 1);
-  for(int p = 0; p < (int)prim.size(); p++) {
+  auto prim = vector<unsigned char>(MAX/2, 1);
+  for(unsigned int p = 0; p < (unsigned int)prim.size(); p++) {
     if(!prim[p]) continue;
 
-    int actual = p*2+3;
+    auto actual = p*2+3;
     
     for(auto const& possible : make_keys(actual)) {
       values.insert(std::make_pair(possible, actual));
@@ -119,7 +119,7 @@ int main() {
 
     //potential off-by-one error and subsequent nasal demons here
     if(actual <= SQRT_MAX)
-    for(int np = (actual * actual)/2 - 1; np < (int)prim.size(); np += actual)
+    for(auto np = (actual * actual)/2 - 1; np < (unsigned int)prim.size(); np += actual)
       prim[np] = 0;
   }
   cout << "Nothing!" << endl;
